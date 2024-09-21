ed89f91fe75181dffa683084f079a39b761197f8
muniznicole <nicoleamuniz@gmail.com>
2024-09-20 23:16:20 -0300

    Create Municipio

---
+++ src/app/components/municipio/municipio-form/municipio-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-municipio-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './municipio-form.component.html',
+  styleUrl: './municipio-form.component.css'
+})
+export class MunicipioFormComponent {
+
+}
