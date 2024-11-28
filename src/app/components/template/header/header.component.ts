90318ed3ae5943ad48e0fb7876f8f7a1661bd5eb
muniznicole <nicoleamuniz@gmail.com>
2024-11-28 17:44:33 -0300

    Create Template

---
+++ src/app/components/template/header/header.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { SidebarService } from '../../../services/sidebar.service';
+import { Usuario } from '../../../models/usuario.model';
+import { AuthService } from '../../../services/auth.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbar, MatIcon, MatBadge, MatButton, MatIconButton, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  usuarioLogado: Usuario | null = null;
+  private subscription = new Subscription();
+
+  constructor(
+    private sidebarService: SidebarService,
+    private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    this.subscription.add(this.authService.getUsuarioLogado().subscribe(
+      usuario => this.usuarioLogado = usuario
+    ));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  clickMenu() {
+    this.sidebarService.toggle();
+  }
+
+  deslogar() {
+    this.authService.removeToken();
+    this.authService.removeUsuarioLogado();
+  }
+
+}
